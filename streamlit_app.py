771cc105900ac64ade157c92321ff855777a111d
Adarsh Gautam <37012836+adarsh-gautam@users.noreply.github.com>
2023-06-23 02:12:58 +0530

    Update streamlit_app.py
    added comments
    added pandas dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,17 @@
+import pandas as pd
 import streamlit
 
+# tile
 streamlit.title('My Parent\'s new Healthy Diner!!!')
 
+# header 1
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
 
+# header 2
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
